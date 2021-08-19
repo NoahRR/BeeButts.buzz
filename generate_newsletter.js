56b7beb61da02d8255abb2b459ff1543e7d233d8
Noah Ray Roberts <noahray216@gmail.com>
2021-08-18 20:32:43 -0500

    finished whole process - now just need to finalize the newsletter design b4 I pass in variables and such

--- generate_newsletter.js
+++ generate_newsletter.js
@@ -1,5 +1,12 @@
+//
+// GENERATING THE BEE BUTTS WEEKLY NEWSLETTER
+//
+
+require('dotenv').config();
+
 // GET NEWEST BEE BUTTS DATA FROM REDDIT
 const fetch = require('node-fetch');
+var bee_data = [];
 
 fetch('https://www.reddit.com/r/beebutts/top/.json?limit=10&t=week')
   .then(response => response.json())
@@ -8,60 +15,75 @@ fetch('https://www.reddit.com/r/beebutts/top/.json?limit=10&t=week')
 
         data['data']['children'].forEach(element => {
             // console.log(element['data'])
-            console.log(element['data']['subreddit_name_prefixed'])
-            console.log(element['data']['title'])
-            console.log(element['data']['url_overridden_by_dest'])
-            console.log(element['data']['author'])
-            console.log(element['data']['url'])
-            console.log('https:/www.reddit.com' + element['data']['permalink'])
-            console.log('\n')
+            bee_post_data = {};
+            bee_post_data['title'] = element['data']['title'];
+            bee_post_data['sub_reddit'] = element['data']['subreddit_name_prefixed'];
+            bee_post_data['image_url'] = element['data']['url_overridden_by_dest'];
+            bee_post_data['author'] = element['data']['author'];
+            bee_post_data['image_url_2'] = element['data']['url'];
+            bee_post_data['post_url'] = 'https:/www.reddit.com' + element['data']['permalink'];
+            bee_data.push(bee_post_data);
         });
-        
+        console.log(bee_data);
+
+        // INSERT NEW DATA INTO HTML TEMPLATE
+        const mustache   = require('mustache');
+        const fs = require('fs'); 
+
+        var content = fs.readFileSync("newletter_template.html","utf-8");
+        // var bee_data = {
+        //   formatted: {
+        //     latitude: 0,
+        //     longitude:0
+        //   },
+        //   formattedDate:"01/01/1990"
+        // }
+        // var new_newsletter_html = mustache.render(content, bee_data);
+        // end section
+
+
+        // MAIL CHIMP CAMPAIGN CREATE, SET CONTENT, SEND
+        const client = require('@mailchimp/mailchimp_marketing');
+
+        client.setConfig({
+          apiKey: process.env.MAILCHIMP_API_KEY,
+          server: process.env.MAILCHIMP_SERVER,
+        });
+
+        var todayDate = new Date().toISOString().slice(0, 10);
+
+        const run = async () => {
+          const response = await client.campaigns.create(
+            {
+              type: "regular",
+              recipients: {
+                list_id: process.env.MAILCHIMP_AUDIENCE_LIST_ID
+              },
+              tracking: {
+                opens: true
+              },
+              settings: {
+                title: todayDate + " Bee Butts Newsletter",
+                subject_line: "Your weekly good vibes have arrived!!",
+                preview_text: "Bzz Bzzt! Open me to view bee butts :D",
+                from_name: "The Bee Butts Group",
+                reply_to: "noah@beebutts.buzz"
+              },
+            }
+          );
+
+          new_campaign_id = response['id'];
+
+          const response_set_content = await client.campaigns.setContent(new_campaign_id, {
+            html: content
+          });
+
+          const response_send = await client.campaigns.send(new_campaign_id);
+
+        };
+        // end section
+
+        run();
+
       });
-
-// INSERT NEW DATA INTO HTML TEMPLATE
-// other option is to use mailchimp template - then we can define variables to pass in I believe.. in the docs https://mailchimp.com/developer/marketing/api/campaigns/ .. but prob just do this manually here - create enw file, from bee pro html export - then pass in variables with javascript, and then get the html, and input into api call.
-
-// WILL HAVE TO GENERATE NEW API KEY -- this one was disabled czu it was found online at beebutts github -- need to go and set this to private!!! or find a way to pass this in with .env or something
-
-// MAIL CHIMP CAMPAIGN CREATE, SET CONTENT, SEND
-const client = require('@mailchimp/mailchimp_marketing');
-
-client.setConfig({
-  apiKey: "e5989eab8e9d539b6a5befeda73627d2-us6",
-  server: "us6",
-});
-
-var todayDate = new Date().toISOString().slice(0, 10);
-
-const run = async () => {
-  const response = await client.campaigns.create(
-    {
-      type: "plaintext",
-      recipients: {
-        list_id: "619c7f73dc"
-      },
-      tracking: {
-        opens: true
-      },
-      settings: {
-        title: todayDate + " Bee Butts Newsletter",
-        subject_line: "Your weekly good vibes have arrived!!",
-        preview_text: "Bzz Bzzt! Open me to view bee butts :D",
-        from_name: "The Bee Butts Group",
-        reply_to: "noah@beebutts.buzz"
-      },
-    }
-  );
-
-  new_campaign_id = response['id'];
-
-  const response_set_content = await client.campaigns.setContent(new_campaign_id, {
-    plain_text: "this is the plain text of this email *|UNSUB|*"
-  });
-
-  const response_send = await client.campaigns.send(new_campaign_id);
-
-};
-
-run();
+// end section
