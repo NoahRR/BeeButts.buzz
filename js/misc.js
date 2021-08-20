2ae8056b2f39f4ed2a8377095448c7b31e194e2b
Noah Ray Roberts <noahray216@gmail.com>
2021-08-19 22:03:04 -0500

    all ipad formatting fix

--- js/misc.js
+++ js/misc.js
@@ -2,15 +2,6 @@
 window.addEventListener('DOMContentLoaded',function(){
     
     // hide blob if on ipad
-    // p = navigator.platform;
-
-    // p = MacIntel is what safari on ipad uses... can't use that
-    // if( p == 'iPad' ) {
-    //     document.querySelector('#desktop-blob-main').style.display = 'none';
-    // }
-
-    // var ipad_or_not = navigator.userAgent.match(/iPad/i) != null;
-
     const iPad = !!(navigator.userAgent.match(/(iPad)/)
     || (navigator.platform === "MacIntel" && typeof navigator.standalone !== "undefined"))
 
